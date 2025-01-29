9a6808c2a68e98565832355c080d7f7b58b2a12d
OksanaEvstratova <v.e.oksana@gmail.com>
2025-01-30 00:20:39 +1100

    tests

--- src/js/__tests__/user.test.js
+++ src/js/__tests__/user.test.js
@@ -12,16 +12,16 @@ test('correct', () => {
 
 });
 
-test('not correct', () => {
-  const obj = {
-    name: 'мечник', 
-    health: 10, 
-    level: 2, 
-    attack: 80, 
-    defence: 40};
-
-  expect(orderByProps(obj, ["username", "level"])).toEqual({name: 'мечник', level: 2, attack: 80, defence: 40, health: 10});
-
+test('wrong', () => {
+  expect(() => {
+    const obj = {
+      name: 'мечник', 
+      health: 10, 
+      level: 2, 
+      attack: 80, 
+      defence: 40};
+      orderByProps(obj, ["namename", "level"])
+  
+  }).toThrow(new Error('Неверное свойство'));
 });
- 
  
--- src/js/user.js
+++ src/js/user.js
@@ -7,12 +7,13 @@ export default function orderByProps(obj, propList) {
     if (item in obj) {
       sorted[item] = obj[item];
       sortedKeys.splice(sortedKeys.indexOf(item), 1);
+    } else {
+      throw new Error('Неверное свойство');  
     }
+    
     });
-    sortedKeys.forEach(function(item) {
-    if (item in obj) {
-      sorted[item] = obj[item];
-    }
+    sortedKeys.forEach(function(itemKey) {
+      sorted[itemKey] = obj[itemKey];
     });
     return sorted;
 
